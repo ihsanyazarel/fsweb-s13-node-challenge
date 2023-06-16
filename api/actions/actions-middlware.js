a5e64b82dbe689a9c1ab4c385ccc0c8df02a9fa5
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-06-16 15:46:20 +0300

    completed

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,42 @@
 // eylemlerle ilgili ara katman yazılımları yazın
+const actionsModel = require("./actions-model");
+const projectsModel = require("../projects/projects-model");
+
+const actIdValidation = async (req,res,next)=>{
+    try {
+        const action = await actionsModel.get(req.params.id);
+        action ? (req.action = action ,next()): res.status(404).json({message : "Belirtilen id'li action bulunamadı"});
+    } catch (error) {
+        next(error);
+    }
+}
+
+const payloadValidation = async (req,res,next)=>{
+    try {
+        const {project_id, description, notes} = req.body;
+        if(!project_id || !description || !notes){
+            res.status(400).json({message: "project_id, description ve notes alanlarını eksiksiz giriniz"});
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+const existProjectVld = async (req,res,next)=>{
+    try {
+        const {project_id} = req.body;
+        const project = await projectsModel.get(project_id);
+        project ? next() : res.status(404).json({message : "Belirtilen id'li proje bulunamadı"});
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+module.exports = {
+    actIdValidation,
+    payloadValidation,
+    existProjectVld
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,15 +1,46 @@
 // "eylem" routerını buraya yazın
 const router = require("express").Router();
+const { actIdValidation, payloadValidation, existProjectVld } = require("./actions-middlware");
 const actionsModel = require("./actions-model");
 
-
-
-
-
-
-
-
-
+router.get("/" , async (req, res, next)=>{
+    try {
+        const allActions = await actionsModel.get();
+        res.json(allActions);
+    } catch (error) {
+        next(error);
+    }
+})
+router.get("/:id" ,actIdValidation, async (req, res, next)=>{
+    try {
+        res.json(req.action);
+    } catch (error) {
+        next(error);
+    }
+})
+router.post("/",payloadValidation,existProjectVld, async (req, res, next)=>{
+    try {
+        const newAction = await actionsModel.insert(req.body);
+        res.status(201).json(newAction);
+    } catch (error) {
+        next(error);
+    }
+})
+router.put("/:id",actIdValidation,payloadValidation,existProjectVld, async (req, res, next)=>{
+    try {
+        const updatedAction = await actionsModel.update(req.params.id, req.body);
+        res.json(updatedAction);
+    } catch (error) {
+        next(error);
+    }
+})
+router.delete("/:id",actIdValidation, async (req, res, next)=>{
+    try {
+        res.json(await actionsModel.remove(req.params.id));
+    } catch (error) {
+        next(error);
+    }
+})
 
 
 module.exports = router;
